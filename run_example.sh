7255b254b62d58faf2e7424ce46d0f0a5689c3c5
rakjin <grampush@gmail.com>
2016-11-18 23:43:15 +0900

    noaudio, readme

--- run_example.sh
+++ run_example.sh
@@ -7,6 +7,7 @@ EXAMPLE=${1:-hello_world}
 (
     cd $(dirname $0)
     blender \
+        -noaudio \
         --background \
         --factory-startup \
         --python examples/example_context.py \
